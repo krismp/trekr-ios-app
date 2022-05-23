e57f0aab6c4cc2d6315b0e9d2e27d8bcf76309be
krismp <kris.mp7777@gmail.com>
2022-05-23 18:33:34 +0700

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Kris on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
